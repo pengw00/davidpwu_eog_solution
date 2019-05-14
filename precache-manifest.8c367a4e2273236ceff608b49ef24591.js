47fbbce489856ceb2ef860f6d2eaf58464373463
pengw00 <water685@hotmail.com>
2019-05-14 16:44:01 -0400

    Updates

---
+++ precache-manifest.8c367a4e2273236ceff608b49ef24591.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c579d99054028879a9a0",
+    "url": "/pengw00/davidpwu_eog_solution.git/static/js/main.c579d990.chunk.js"
+  },
+  {
+    "revision": "efe6f9ae7889e200056a",
+    "url": "/pengw00/davidpwu_eog_solution.git/static/css/1.08c308e4.chunk.css"
+  },
+  {
+    "revision": "efe6f9ae7889e200056a",
+    "url": "/pengw00/davidpwu_eog_solution.git/static/js/1.efe6f9ae.chunk.js"
+  },
+  {
+    "revision": "a8b07cd50e93f9c985df",
+    "url": "/pengw00/davidpwu_eog_solution.git/static/js/runtime~main.a8b07cd5.js"
+  },
+  {
+    "revision": "a9f524fbc71bad5daa3b0b75fff66902",
+    "url": "/pengw00/davidpwu_eog_solution.git/index.html"
+  }
+];
